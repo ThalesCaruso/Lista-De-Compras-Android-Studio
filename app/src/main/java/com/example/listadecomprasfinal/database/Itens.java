18517fb959e83c616b633bf76e12d9ac18485cf3
alexandre-malabarba <alexandre221@gmail.com>
2022-12-07 02:46:56 -0300

    it

--- app/src/main/java/com/example/listadecomprasfinal/database/Itens.java
+++ app/src/main/java/com/example/listadecomprasfinal/database/Itens.java
@@ -1,4 +1,24 @@
 package com.example.listadecomprasfinal.database;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Itens {
+
+    @PrimaryKey (autoGenerate = true)
+    public int uid;
+
+    @ColumnInfo(name = "NomeItem")
+    public String NomeItem;
+
+    @ColumnInfo(name = "IdCategoria")
+    public String IdCategoria;
+
+    @ColumnInfo(name = "Completado")
+    public boolean Completado;
+
+
+
 }
