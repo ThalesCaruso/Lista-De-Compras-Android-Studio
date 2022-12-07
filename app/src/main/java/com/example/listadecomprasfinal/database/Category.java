998d2c456a49601b0c737694f04a420d0de247ab
alexandre-malabarba <alexandre221@gmail.com>
2022-12-07 02:26:02 -0300

    cat

--- app/src/main/java/com/example/listadecomprasfinal/database/Category.java
+++ app/src/main/java/com/example/listadecomprasfinal/database/Category.java
@@ -1,4 +1,20 @@
 package com.example.listadecomprasfinal.database;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Category {
+
+    @PrimaryKey(autoGenerate = true)
+    public int uid;
+
+    @ColumnInfo(name = "NomeCategoria")
+    public String NomeCategoria;
+
+
+
+
+
 }
